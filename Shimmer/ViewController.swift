01833e797d65291a78f0c04c3da134f742d0488b
kimtaesu <kimtaesoo188@gmail.com>
2019-09-03 22:28:15 +0900

    first commit

---
+++ Shimmer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shimmer
+//
+//  Created by tskim on 03/09/2019.
+//  Copyright © 2019 hucet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
